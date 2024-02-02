514d59d8f22101c268de81cadefba1c8dd3fa42c
Kirtan-64 <143480445+Kirtan-64@users.noreply.github.com>
2024-02-02 04:19:18 +0000

    First react test project using vite

---
+++ test/src/App.jsx
@@ -0,0 +1,14 @@
+import Tile from "./Tile"
+function App() {
+  const number1 = 1
+
+  return (
+    <>
+    <h1>This is from app</h1>
+    <Tile number = {1} />
+    <Tile number = {2} />
+    </>
+  )
+}
+
+export default App
---
+++ test/src/Tile.jsx
@@ -0,0 +1,7 @@
+const Tile = ({number}) => {
+    return (
+        <h1>This is a tile {number}</h1>
+    )
+}
+
+export default Tile;
